de4c0d5faab17b31e156124e8c3784e3fc6d579f
jeffjose95 <Jeff_jose95@yahoo.com>
2017-12-02 20:24:41 -0800

    Added a project to my projects

--- js/gameUI.js
+++ js/gameUI.js
@@ -1,25 +1,10 @@
-var canvas = document.querySelector('canvas');
+var canvas = document.getElementbyId('BackgroundCanvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
+var numberOfTabs = 4;
 
-var maxWidth = 100;
-var maxHeight = 100;
-var loopcounter = Math.random() * 100;
-
-c.font ='sans-serif'
-c.fillText("Under Devekopment", canvas.width/2, canvas.height/2,canvas.width/2, canvas.height/2);
-c.fillStyle = 'red'
-c.strokeText("Under Devekopment", canvas.width/2, canvas.height/2,canvas.width/2, canvas.height/2);
-c.strokeStyle = 'blue';
-c.stroke();
-//   Setting up a line
-// c.beginPath();
-// c.moveTo(x,y);
-// c.lineTo(x,y);
-// c.strokeStyle = ''
-// c.stroke();
 
 var mouseMove = {
   x: undefined,
@@ -28,7 +13,8 @@ var mouseMove = {
 
 var mouseClicked = {
   x: undefined,
-  y: undefined
+  y: undefined,
+  tabClickedLast: undefined
 }
 
 window.addEventListener('mousemove',function(event) {
@@ -44,79 +30,77 @@ window.addEventListener('click',function(event)
   console.log(mouseClicked);
 })
 
-// function tab(x, y){
-// this.x = x;
-// this.y = y;
-// this.width = width;
-// this.height = height;
-// this.color = color;
-//
-//   this.draw = function(){
-//     c.fillRect(this.x,this.y,this.width,this.height);
-//     c.fillStyle = this.color;
-//   }
-//
-//   this.update = function(){
-//
-//     if(this.x + (this.width/2) > mouseMove.x && this.x - (this.width/2) < mouseMove.x && this.y + (this.height/2) > mouseMove.y && this.y - (this.height/2) < mouseMove.y)
-//     {
-//       var diffX = mouseMove.x - this.x;
-//       var diffY = mouseMove.y - this.y;
-//
-//       this.x = mouseMove.x - diffX;
-//       this.y = mouseMove.y - diffY;
-//
-//     }
-//     else {
-//       if (this.x + (this.width/2) > canvas.width || this.x- (this.width/2) < 0)
-//       {
-//         this.speedX = -this.speedX;
-//       }
-//
-//       if (this.y + (this.height/2) > canvas.height || this.y- (this.height/2) < 0)
-//       {
-//         this.speedY = -this.speedY;
-//       }
-//
-//     }
-//
-//     this.draw();
-//   }
-// }
+var tabArray = [];
 
-// var squareArray = [];
-//
-// for(var i = 0; i< loopcounter; i++)
-// {
-// //Setting fill color to a number without any decimal values
-// var r = (Math.random() * 255) | 0;
-// var g = (Math.random() * 255) | 0;
-// var b = (Math.random() * 255) | 0;
-// var a = 1;
-// var color ='rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
-//
-//   //Setting up a rectangle
-//   var x = (Math.random() * canvas.width) | 0;
-//   var y = (Math.random() * canvas.height) | 0;
-//   var width = (Math.random() * maxWidth) | 0;
-//   var height = (Math.random() * maxHeight) | 0;
-//   // console.log(r);
-//   // console.log(g);
-//   // console.log(b);
-//   // console.log(a);
-//
-//   var speedX = ((Math.random() - 0.5) * 10) | 0;
-//   var speedY = ((Math.random() - 0.5) * 10) | 0;
-// squareArray.push(new square(x,y,width,height,color,speedX, speedY));
-// }
 
-// function animate(){
-//  c.clearRect(0,0,canvas.width,canvas.height);
-//
-// for(var i = 0; i< squareArray.length; i++)
-//   {
-//       squareArray[i].update();
-//   }
-//   requestAnimationFrame(animate);
-// }
-//animate();
+function animate(){
+ c.clearRect(0,0,canvas.width,canvas.height);
+
+for(var i = 0; i< tabArray.length; i++)
+  {
+      tabArray[i].update();
+  }
+  mouseClicked.x = undefined;
+  mouseClicked.y = undefined;
+  requestAnimationFrame(animate);
+}
+
+function tab(color, linkNumber, link){
+
+this.width = 50;
+this.height = canvas.height / numberOfTabs;
+this.x = 0;
+this.y = this.height*linkNumber;
+this.color = color;
+this.link = link;
+
+  this.draw = function(){
+    c.fillRect(this.x,this.y,this.width,this.height);
+    c.fillStyle = this.color;
+  }
+
+  this.update = function(){
+
+    if(mouseClicked.tabClickedLast == this)
+    {
+      if(this.width < 100)
+      {
+        this.width++;
+      }
+    }
+    else {
+      if (this.x + (this.width) > mouseClicked.x && this.x < mouseClicked.x && this.y + this.height > mouseClicked.y && this.y < mouseClicked.y)
+      {
+        mouseClicked.tabClickedLast = this;
+      }
+
+      if (this.x + (this.width) > mouseMove.x && this.x < mouseMove.x && this.y + this.height > mouseMove.y && this.y < mouseMove.y)
+      {
+        if(this.width < 75)
+        {
+          this.width++;
+        }
+      }
+      else if (this.width> 50)
+      {
+        this.width--;
+      }
+    }
+
+    this.draw();
+  }
+}
+
+var color ='rgba(' + 41 + ',' + 128 + ',' + 185 + ',' + 1 + ')';
+tabArray.push(new tab(color,0));
+
+color ='rgba(' + 46 + ',' + 204 + ',' + 113 + ',' + 1 + ')';
+tabArray.push(new tab(color, 1));
+
+color ='rgba(' + 230 + ',' + 126 + ',' + 34 + ',' + 1 + ')';
+tabArray.push(new tab(color, 2));
+
+color ='rgba(' + 243 + ',' + 156 + ',' + 18 + ',' + 1 + ')';
+tabArray.push(new tab(color, 3));
+
+animate();
